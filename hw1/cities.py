7ee2f4f3decf7d39bf1a67960eb2d2a476845e6c
Parsa <106576103+curedbylethe@users.noreply.github.com>
2023-10-26 21:02:19 +0330

    fetch the data using multithreading

---
+++ hw1/cities.py
@@ -0,0 +1,9 @@
+from hw1.city import City
+
+cities = [
+    City("Tehran", 35.7219, 51.3347),
+    City("Esfahan", 32.6539, 51.6660),
+    City("Bushehr", 29.5916, 50.8279),
+    City("Tabriz", 38.0792, 46.2887),
+    City("Ahvaz", 31.3183, 48.6706),
+]
