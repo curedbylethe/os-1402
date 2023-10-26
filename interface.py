b2c313ca580828330f58a75929c6bb5c7442c1da
Parsa <106576103+curedbylethe@users.noreply.github.com>
2023-10-26 21:06:45 +0330

    improve interface code

--- interface.py
+++ interface.py
@@ -10,11 +10,22 @@ class Interface:
         for i in range(self.number_of_homeworks):
             print(f"{i + 1}. HW{i + 1}")
 
-    def print_problems(self, homework_number):
+    def print_problems(self, homework_number: int):
         print(f"HW{homework_number} had {len(self.homeworks[homework_number - 1])} " 
               "problems, which one are you interested in?")
         for i in range(len(self.homeworks[homework_number - 1])):
             print(f"{i + 1}. {self.homeworks[homework_number - 1][i]}")
+
+    @classmethod
+    def handle_hw1(cls, problem_number: int):
+        if problem_number == 1:
+            print("Fetching weather for Iranian cities...")
+            from hw1.fetch import fetch
+            fetch()
+        elif problem_number == 2:
+            print("Generating processes isn't implemented yet!")
+        else:
+            print("Invalid problem number!")
 
     def run(self):
         self.print_homeworks()
@@ -22,13 +33,6 @@ class Interface:
         self.print_problems(homework_number)
         problem_number = int(input("Enter the number of the problem: "))
         if homework_number == 1:
-            if problem_number == 1:
-                print("Fetching weather for Iranian cities...")
-                from hw1.fetch import fetch
-                fetch()
-            elif problem_number == 2:
-                print("Generating processes isn't implemented yet!")
-            else:
-                print("Invalid problem number!")
+            self.handle_hw1(problem_number)
         else:
             print("Invalid homework number!")
